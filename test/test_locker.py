398c47944aa2daf5b3ba3851ae3ccec7b984071b
Luttik <dtluttik@gmail.com>
2020-06-01 22:12:29 +0200

    Added tests to get codecov to 100% fixed found error.

--- test/test_locker.py
+++ test/test_locker.py
@@ -1,4 +1,9 @@
+from unittest.mock import patch, mock_open
+
+from pytest import raises
+
 from test_locker import Locker
+from test_locker.fixtures import UserDidNotAcceptDataException
 
 
 def test_locker(locker: Locker) -> None:
@@ -12,6 +17,32 @@ def test_locker_with_name(locker: Locker) -> None:
     value = "test string 2"
     locker.lock(value, "this is my name")
     with open(
-        ".pytest_locker/test.test_locker.test_locker_with_name.this is my name.txt"
+            ".pytest_locker/test.test_locker.test_locker_with_name.this is my name.txt"
     ) as lock:
         assert lock.read() == value
+
+
+@patch('builtins.input', lambda *args: 'n')
+def test_locker_without_file(locker: Locker) -> None:
+    with raises(UserDidNotAcceptDataException):
+        locker.lock("File does not exist")
+
+
+@patch('builtins.input', lambda *args: 'n')
+def test_locker_with_wrong_data(locker: Locker) -> None:
+    with raises(UserDidNotAcceptDataException):
+        locker.lock("File has different context")
+
+
+@patch('builtins.input', lambda *args: 'y')
+def test_locker_without_file_accepted(locker: Locker) -> None:
+    io_mocker = mock_open()
+    with patch('io.open', io_mocker):
+        value = "File has different context"
+        locker.lock(value)
+        write_calls = [call for call in io_mocker.mock_calls if call[0] == '().write']
+        assert len(write_calls) == 1
+        write_call = write_calls[0]
+        assert len(write_call[1]) == 1
+        assert len(write_call[2]) == 0
+        assert write_call[1][0] == "File has different context"
