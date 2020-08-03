4860bdaf7b2f0699432019d0c668be93583215d3
Daan Luttik <dtluttik@gmail.com>
2020-08-03 16:48:13 +0200

    prettier prints (#3)
    Improved console output. Added a test that locks the output.

--- src/pytest_locker/fixtures.py
+++ src/pytest_locker/fixtures.py
@@ -85,17 +85,22 @@ class Locker:
                     SEPERATOR,
                     "\nDIFF:",
                     SEPERATOR,
-                    *difflib.unified_diff(
-                        old_data.splitlines(True),
-                        new_data.splitlines(True),
-                        fromfile=f"old value: ({path})",
-                        tofile="new value",
-                    ),
+                    self.get_diff(old_data, new_data),
                     SEPERATOR,
                 ]
             )
         )
         self.__write_if_accepted(new_data, path, "Do you accept the new data? (y|n)")
+
+    def get_diff(self, old_data: str, new_data: str):
+        diff = difflib.unified_diff(
+            old_data.splitlines(True), new_data.splitlines(True),
+        )
+
+        # noinspection PyUnusedLocal
+        ignored_from_file, ignored_to_file = next(diff), next(diff)
+
+        return "".join(diff)
 
     def __write_if_accepted(
         self, data: str, lock_path: Path, acceptance_request: str = None
