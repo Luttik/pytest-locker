c2fb2ddc017fbc5599aae0829139a137bc0ce8e0
Luttik <dtluttik@gmail.com>
2020-08-11 11:38:32 +0200

    Updated dependencies, updated codes accordingly

--- tasks.py
+++ tasks.py
@@ -13,7 +13,7 @@ make_setup = simple_task(name="make-setup", commands="dephell deps convert")
 
 format = simple_task(name="format", commands="inv black isort")
 black = simple_task(name="black", commands="black -q .")
-isort = simple_task(name="isort", commands="isort -rc .")
+isort = simple_task(name="isort", commands="isort .")
 
 flake8 = simple_task(name="flake8", commands="flake8 .")
 
