e75148d84641d2219b9431482d74b7a9923c8df9
luttik <dtluttik@gmail.com>
2021-10-26 22:52:09 +0200

    Updated lint to fix wrong black configuration

--- tasks.py
+++ tasks.py
@@ -12,9 +12,11 @@ def simple_task(name: str, commands: str) -> task:
 make_setup = simple_task(name="make-setup", commands="dephell deps convert")
 
 format = simple_task(name="format", commands="inv black isort")
-lint = simple_task(name="lint", commands="inv flake8 mypy")
+lint = simple_task(name="lint", commands="inv flake8 mypy black-check isort-check")
 black = simple_task(name="black", commands="black -q .")
+black_check = simple_task(name="black-check", commands="black -q --check .")
 isort = simple_task(name="isort", commands="isort .")
+isort_check = simple_task(name="isort-check", commands="isort . --check --diff")
 
 flake8 = simple_task(name="flake8", commands="flake8 .")
 mypy = simple_task(name="mypy", commands="mypy ")
