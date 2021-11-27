22e562c86c7724117e60ebeea7caba3b59c15d16
luttik <dtluttik@gmail.com>
2021-11-27 22:52:29 +0100

    Work in progress

--- test/test_locker.py
+++ test/test_locker.py
@@ -12,7 +12,9 @@ def test_str_locker(locker: Locker) -> None:
     value = "test string 1"
     locker.lock(value)
     rootdir = Path(locker.request.session.fspath).absolute()
-    with open(f"{rootdir}/.pytest_locker/test.test_locker.test_locker.1.txt") as lock:
+    with open(
+        f"{rootdir}/.pytest_locker/test.test_locker.test_str_locker.1.txt"
+    ) as lock:
         assert lock.read() == f"{value}"
 
 
@@ -22,7 +24,7 @@ def test_str_locker_with_name(locker: Locker) -> None:
     rootdir = Path(locker.request.session.fspath).absolute()
     with open(
         f"{rootdir}/.pytest_locker/test.test_locker"
-        ".test_locker_with_name.this is my name.txt"
+        ".test_str_locker_with_name.this is my name.txt"
     ) as lock:
         assert lock.read() == f"{value}"
 
@@ -69,7 +71,7 @@ def test_locker_without_file_accepted(locker: Locker) -> None:
         write_call = write_calls[0]
         assert len(write_call[1]) == 1
         assert len(write_call[2]) == 0
-        assert write_call[1][0] == '"File has different context"'
+        assert write_call[1][0] == "File has different context"
 
 
 def test_multiline_diff(locker: Locker, capsys: CaptureFixture) -> None:
